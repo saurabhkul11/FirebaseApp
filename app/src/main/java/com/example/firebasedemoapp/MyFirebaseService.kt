ff5bda6c1c3ff53c4584a8193caf028f6539aa8c
vzk3ck9v <saurabh.kulkarni@neosoftmail.com>
2023-09-27 14:47:32 +0530

    firebase integration

--- app/src/main/java/com/example/firebasedemoapp/MyFirebaseService.kt
+++ app/src/main/java/com/example/firebasedemoapp/MyFirebaseService.kt
@@ -1,5 +1,7 @@
 package com.example.firebasedemoapp
 
+import android.app.NotificationManager
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -8,10 +10,18 @@ class MyFirebaseService:FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-        message.notification.hashCode()
+        message.notification.let {
+            sendnotification(it?.body.toString())
+        }
+
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+    }
+
+    fun sendnotification( str:String){
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+        notificationManager.sendNotification(str, applicationContext)
     }
 }
